e72d78b1f88d44b1b3613aaa1de57400aaf9fb53
StrahiCraft <strahi04@gmail.com>
2026-02-10 11:54:07 +0100

    added hibernate dependency

---
+++ src/main/java/server/AccountValidation.java
@@ -0,0 +1,11 @@
+package server;
+
+public class AccountValidation {
+    public static boolean LoginValid(String username, String password) {
+        return true;
+    }
+
+    public static boolean RegisterValid(String username, String password) {
+        return true;
+    }
+}
